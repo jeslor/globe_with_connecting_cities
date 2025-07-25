9b60cabd805c7c393d4e17fdd5e0eba2235e6f59
Jeslor Ssozi <jbestius@gmail.com>
2025-07-25 20:35:24 +0200

    add the globe and connecting cities

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import GlobeWithMultipleCitiesConnecting from "./components/GlobeWithMultipleCitiesConnecting";
-import SpaceCanvas from "./components/SpaceCanvas";
 
 const App = () => {
   return (
-    <div className="h-screen w-screen relative overflow-hidden">
-      <div className="absolute h-full w-full bg-black overflow-hidden">
-        <SpaceCanvas />
-      </div>
+    <div className="h-screen w-screen relative overflow-hidden bg-black">
       <div className="absolute h-full w-full z-[2] top-0 left-0 flex items-center justify-center">
         <GlobeWithMultipleCitiesConnecting />
       </div>
