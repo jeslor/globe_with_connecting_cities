e49e746a875911744514c87ae8b3b69eb87e89fd
Jeslor Ssozi <jbestius@gmail.com>
2025-07-25 20:36:37 +0200

    add the sky canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import GlobeWithMultipleCitiesConnecting from "./components/GlobeWithMultipleCitiesConnecting";
+import SpaceCanvas from "./components/SpaceCanvas";
 
 const App = () => {
   return (
-    <div className="h-screen w-screen relative overflow-hidden bg-black">
+    <div className="h-screen w-screen relative overflow-hidden ">
+      <div className="absolute h-full w-full z-[-1] top-0 left-0">
+        <SpaceCanvas />
+      </div>
       <div className="absolute h-full w-full z-[2] top-0 left-0 flex items-center justify-center">
         <GlobeWithMultipleCitiesConnecting />
       </div>
